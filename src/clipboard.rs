6050583125c647f0351831d351976a83a4c216a2
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2024-05-18 19:19:27 +0100

    Allow pasting transparent bitmaps

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -7,6 +7,12 @@
 
 use std::{io::Cursor, num::NonZeroU32};
 
+use clipboard_win::{
+	formats::{Bitmap, CF_BITMAP},
+	Getter,
+};
+use embedded_graphics::pixelcolor::RgbColor;
+
 pub struct Clipboard {
 	custom_format: NonZeroU32,
 	png_format: NonZeroU32,
@@ -55,6 +61,39 @@ impl Clipboard {
 			png_reader.next_frame(&mut image_buffer).ok()?;
 
 			return Some(ClipboardData::Image { dimensions: [width, height], data: image_buffer });
+		} else if clipboard_win::is_format_avail(CF_BITMAP) {
+			let mut data = Vec::new();
+			clipboard_win::raw::open().ok()?;
+			Bitmap.read_clipboard(&mut data).ok()?;
+			clipboard_win::raw::close().ok()?;
+
+			let bmp = tinybmp::Bmp::<embedded_graphics::pixelcolor::Rgb888>::from_slice(&data).ok()?;
+			let width = bmp.as_raw().header().image_size.width;
+			let height = bmp.as_raw().header().image_size.height;
+			let mut data = Vec::<u8>::with_capacity(width as usize * height as usize * 4);
+			let bmp_iterator = bmp.as_raw().image_data().chunks_exact(width as usize * 4);
+
+			// If the bitmap data is upside down, we interpret it in reverse by row.
+			if bmp.as_raw().header().row_order == tinybmp::RowOrder::BottomUp {
+				for row in bmp_iterator.rev() {
+					data.extend(row)
+				}
+			} else {
+				for row in bmp_iterator {
+					data.extend(row)
+				}
+			}
+
+			// We premultiply RGB by transparency to prevent the 'white border' effect.
+			// For future reference: this might not be a good idea for the general case, but it works nicely
+			// for Windows' legacy snipping tool.
+			for (chunk, pixel) in data.chunks_exact_mut(4).zip(bmp.pixels()) {
+				chunk[0] = (pixel.1.r() as f64 * chunk[3] as f64 / 255.) as u8;
+				chunk[1] = (pixel.1.g() as f64 * chunk[3] as f64 / 255.) as u8;
+				chunk[2] = (pixel.1.b() as f64 * chunk[3] as f64 / 255.) as u8;
+			}
+
+			return Some(ClipboardData::Image { dimensions: [width, height], data });
 		}
 		None
 	}
